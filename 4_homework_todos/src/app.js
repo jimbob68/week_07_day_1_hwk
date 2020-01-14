6e5451f524cff701f436c3cc1af80321956f7a74
James Fraser <jimbob4t@virginmedia.com>
2020-01-14 09:00:56 +0000

    homework completed

--- 4_homework_todos/src/app.js
+++ 4_homework_todos/src/app.js
@@ -2,6 +2,28 @@ import Vue from 'vue';
 
 document.addEventListener('DOMContentLoaded', () => {
   new Vue ({
-    el: "#app"
-  })
-})
+    el: "#app",
+    data: {
+      tasks: [
+        {name: "Wash the car ", isCompleted: true},
+        {name: "Do the dishes ", isCompleted: false},
+        {name: "Get the shopping ", isCompleted: false},
+        {name: "Cut the grass ", isCompleted: false},
+      ],
+      newTask: ""
+    },
+    methods: {
+      saveNewTask: function(task) {
+        this.tasks.push({
+          name: this.newTask,
+          isCompleted: false
+        });
+        this.newTask = "";
+      },
+        taskCompleted: function(index) {
+        this.tasks[index].isCompleted = true;
+      }
+
+    }
+  });
+});
